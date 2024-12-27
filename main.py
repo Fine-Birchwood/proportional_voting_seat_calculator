827fc030170a2f807eac08130895b5cbd030673f
Fine-Birchwood <fredrik.segersteen@gmail.com>
2024-12-27 19:11:55 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+number_of_seats = int(input('How many seats are there in the current board? '))
+number_of_elected_parties = int(input('How many parties have been elected to the current board? '))
+
+print('The board has', number_of_seats, 'seats')
+print('The board has', number_of_elected_parties, 'elected parties')
+
